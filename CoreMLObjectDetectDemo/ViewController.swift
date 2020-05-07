09a52ca51d081675f12a0044a47e89283fc326fa
Bharat <bharat000k@gmail.com>
2020-05-08 00:19:04 +0530

    Initial Commit

---
+++ CoreMLObjectDetectDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLObjectDetectDemo
+//
+//  Created by Bharat Khatke on 08/05/20.
+//  Copyright © 2020 Bharat Khatke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
